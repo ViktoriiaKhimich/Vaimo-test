5ee3e7c1cd0473fcce92c0f47525537fe6c5840a
Вікторія <76106945+ViktoriiaKhimich@users.noreply.github.com>
2021-07-12 16:33:08 +0300

    finished mobile-menu mark-up and logic

--- js/footer-menu.js
+++ js/footer-menu.js
@@ -4,15 +4,22 @@
         icons: document.querySelectorAll('.open-list-icon')
     };
 
-    refs.openArrow.forEach((item, index) => {
-        item.addEventListener('click', function (e) {
-            if (e.target === item) {
-                refs.icons.forEach((icon, idx) => {
-                    if (index === idx) {
-                        icon.classList.toggle('is-list-form-shown')
-                    }
-                })
-            }
-        })
+    refs.icons.forEach((icon, index) => {
+        icon.addEventListener('click', (e) => toggleClass(e, refs.icons, icon, index)
+        )
     })
+
+    refs.openArrow.forEach((item, index) => {
+        item.addEventListener('click', (e) => toggleClass(e, refs.icons, item, index))
+    })
+
+    function toggleClass(e, nodeList, item, index) {
+        if (e.target === item) {
+            nodeList.forEach((node, idx) => {
+                if (idx === index) {
+                    node.classList.toggle('is-list-form-shown')
+                }
+            })
+        }
+    }
 })();
--- js/menu.js
+++ js/menu.js
@@ -3,22 +3,52 @@
         openMenuBtn: document.querySelector('[data-menu-open]'),
         closeMenuBtn: document.querySelector('[data-menu-close]'),
         menu: document.querySelector('[data-menu]'),
-        openSearchBtn: document.querySelector('[data-search-open]'),
-        closeSearchBtn: document.querySelector('[data-search-close]'),
-        search: document.querySelector('[search-menu]'),
+        openSecondMenu: document.querySelector('.women'),
+        secondMenu: document.querySelector('.second-mob-menu'),
+        openThirdMenu: document.querySelector('.clothing'),
+        thirdMenu: document.querySelector('.third-mob-menu'),
+        goBackToSecond: document.querySelector('.go-back-btn'),
+        openSearchBtn: document.querySelector('.search-icon'),
+        // closeSearchBtn: document.querySelector('[data-search-close]'),
+        search: document.querySelector('.search-form'),
     };
 
-    refs.openMenuBtn.addEventListener('click', toggleModal);
-    refs.closeMenuBtn.addEventListener('click', toggleModal);
+    console.log(refs.openSearchBtn);
+
+    refs.openMenuBtn.addEventListener('click', toggleFirstMenu);
+    refs.closeMenuBtn.addEventListener('click', closeMenu);
+
+    refs.openSecondMenu.addEventListener('click', toggleSecondMenu)
+    refs.openThirdMenu.addEventListener('click', toggleThirdMenu)
+
+    refs.goBackToSecond.addEventListener('click', goBackToSecondMenu)
 
     refs.openSearchBtn.addEventListener('click', toggleSearchForm)
-    refs.closeSearchBtn.addEventListener('click', toggleSearchForm)
+    // refs.closeSearchBtn.addEventListener('click', toggleSearchForm)
 
     function toggleSearchForm() {
         refs.search.classList.toggle('is-search-form-hidden')
     }
 
-    function toggleModal() {
+    function goBackToSecondMenu() {
+        refs.thirdMenu.classList.toggle('is-menu-hidden')
+    }
+
+    function closeMenu() {
+        refs.thirdMenu.classList.toggle('is-menu-hidden');
+        refs.secondMenu.classList.toggle('is-menu-hidden');
         refs.menu.classList.toggle('is-menu-hidden');
+    }
+
+    function toggleFirstMenu() {
+        refs.menu.classList.toggle('is-menu-hidden')
+    }
+
+    function toggleSecondMenu() {
+        refs.secondMenu.classList.toggle('is-menu-hidden');
+    }
+
+    function toggleThirdMenu() {
+        refs.thirdMenu.classList.toggle('is-menu-hidden');
     }
 })();
